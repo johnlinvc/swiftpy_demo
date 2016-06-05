2763cca41e0bfd13f124cc376cb5c82ba6164d01
johnlinvc <johnlinvc@gmail.com>
2016-06-05 11:15:12 +0800

    able to compile

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftpy_demo"
+    name: "swiftpy_demo",
+    dependencies: [
+        .Package(url: "https://github.com/johnlinvc/swiftpy.git",
+                majorVersion: 1),
+    ]
 )
